002a29426adb703f7c27715a083a28b6bbe33a1f
santarva <santiago.arangov10@gmail.com>
2025-01-24 20:36:42 -0500

    cambios animacion

--- Ether-no/Assets/Scripts/Player/mov.cs
+++ Ether-no/Assets/Scripts/Player/mov.cs
@@ -1,16 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
+
 
 public class mov : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float velocidadMovimiento;
+
+    private Vector2 direccion;
+
+    private Rigidbody2D rb;
+
+    private float movimientoX;
+    private float movimientoY;
+    public Animator animator;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        movimientoX = Input.GetAxisRaw("Horizontal");
+        movimientoY = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("MovimientoX", movimientoX);
+        animator.SetFloat("MovimientoY", movimientoY);
+
+        if(movimientoX != 0 || movimientoY != 0)
+        {
+            animator.SetFloat("UltimoX", movimientoX);
+            animator.SetFloat("UltimoY", movimientoY);
+        }
+
+
+
+        direccion = new Vector2(movimientoX, movimientoY).normalized;
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + direccion * velocidadMovimiento * Time.fixedDeltaTime);
     }
 }
+
